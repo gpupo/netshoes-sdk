0a514e7ebe70a2e130ecd5a334efe84ae6f53914
Gilmar Pupo <g@g1mr.com>
2016-06-23 17:21:40 -0300

    CS fix

--- src/Entity/ManagerAbstract.php
+++ src/Entity/ManagerAbstract.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of gpupo component
+ * This file is part of gpupo/netshoes-sdk
  * Created by Gilmar Pupo <g@g1mr.com>
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- src/Entity/Product/Sku/Item.php
+++ src/Entity/Product/Sku/Item.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of gpupo component
+ * This file is part of gpupo/netshoes-sdk
  * Created by Gilmar Pupo <g@g1mr.com>
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,18 +17,18 @@ class Item extends EntityAbstract implements EntityInterface
     public function getSchema()
     {
         return  [
-            'sku'  => 'string',
-            'name'  => 'string',
+            'sku'         => 'string',
+            'name'        => 'string',
             'description' => 'string',
-            'color' => 'string',
-            'size' => 'string',
-            'gender' => 'string',
-            'eanIsbn' => 'string',
-            'video' => 'string',
-            'height' => 'string',
-            'width' => 'string',
-            'depth' => 'string',
-            'weight' => 'string',
+            'color'       => 'string',
+            'size'        => 'string',
+            'gender'      => 'string',
+            'eanIsbn'     => 'string',
+            'video'       => 'string',
+            'height'      => 'string',
+            'width'       => 'string',
+            'depth'       => 'string',
+            'weight'      => 'string',
         ];
     }
 }
