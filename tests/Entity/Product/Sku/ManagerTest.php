9100f2b018dfe341d8bb77ea3331af6a4f681c36
Gilmar Pupo <g@g1mr.com>
2016-06-27 11:42:53 -0300

    +tests

--- tests/Entity/Product/Sku/ManagerTest.php
+++ tests/Entity/Product/Sku/ManagerTest.php
@@ -15,18 +15,42 @@ class ManagerTest extends TestCaseAbstract
 {
     protected function getManager($filename = null)
     {
-        $manager = $this->getFactory()->factoryManager('product');
-        $manager->setDryRun($this->factoryResponseFromFixture('fixture/Product/Sku/item.json'));
+        $manager = $this->getFactory()->factoryManager('sku');
+        $manager->setDryRun($this->factoryResponseFromFixture('fixture/Product/Sku/list.json'));
 
         return $manager;
     }
 
+    /**
+     * @testdox Administra operações de SKUs
+     * @test
+     */
     public function testManager()
     {
         $manager = $this->getManager();
 
-        $this->assertInstanceOf('\Gpupo\NetshoesSdk\Entity\Product\Manager', $manager);
+        $this->assertInstanceOf('\Gpupo\NetshoesSdk\Entity\Product\Sku\Manager', $manager);
 
         return $manager;
+    }
+
+    /**
+     * @depends testManager
+     */
+    public function testPossuiObjetoClient($manager)
+    {
+        $this->assertInstanceOf('\Gpupo\NetshoesSdk\Client\Client', $manager->getClient());
+    }
+
+    /**
+     * @testdox Dá Acesso a lista de SKUs de um Product
+     * @depends testManager
+     */
+    public function testFetch($manager)
+    {
+        $list = $manager->findById(14080);
+        $this->assertInstanceOf('\Gpupo\NetshoesSdk\Entity\Product\Sku\SkuCollection', $list);
+
+        return $list;
     }
 }
