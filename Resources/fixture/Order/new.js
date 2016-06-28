d3ed57655e53da6d63eb3eeed09b5563d0dc35c7
Gilmar Pupo <g@g1mr.com>
2016-06-28 17:09:12 -0300

    Order

---
+++ Resources/fixture/Order/new.js
@@ -0,0 +1,205 @@
+{
+  "agreedDate": "2016-06-24T15:01:38.833Z",
+  "businessUnit": "string",
+  "devolutionRequested": true,
+  "exchangeRequested": true,
+  "expands": [
+    "string"
+  ],
+  "links": [
+    {
+      "href": "string",
+      "rel": "string",
+      "templated": true
+    }
+  ],
+  "orderDate": "2016-06-24T15:01:38.833Z",
+  "orderNumber": "string",
+  "orderStatus": "string",
+  "orderType": "string",
+  "originNumber": "string",
+  "originSite": "string",
+  "paymentDate": "2016-06-24T15:01:38.833Z",
+  "shippings": [
+    {
+      "cancellationReason": "string",
+      "country": "string",
+      "customer": {
+        "address": {
+          "city": "string",
+          "complement": "string",
+          "expands": [
+            "string"
+          ],
+          "links": [
+            {
+              "href": "string",
+              "rel": "string",
+              "templated": true
+            }
+          ],
+          "neighborhood": "string",
+          "number": "string",
+          "postalCode": "string",
+          "reference": "string",
+          "state": "string",
+          "street": "string"
+        },
+        "cellPhone": "string",
+        "customerName": "string",
+        "document": "string",
+        "expands": [
+          "string"
+        ],
+        "landLine": "string",
+        "links": [
+          {
+            "href": "string",
+            "rel": "string",
+            "templated": true
+          }
+        ],
+        "recipientName": "string",
+        "stateInscription": "string",
+        "tradeName": "string"
+      },
+      "devolutionItems": [
+        {
+          "brand": "string",
+          "checkInData": "2016-06-24T15:01:38.834Z",
+          "color": "string",
+          "departmentCode": 0,
+          "departmentName": "string",
+          "devolutionData": "2016-06-24T15:01:38.834Z",
+          "devolutionExchangeStatus": "string",
+          "ean": "string",
+          "exchangeProcessCode": 0,
+          "expands": [
+            "string"
+          ],
+          "flavor": "string",
+          "itemId": 0,
+          "links": [
+            {
+              "href": "string",
+              "rel": "string",
+              "templated": true
+            }
+          ],
+          "manufacturerCode": "string",
+          "name": "string",
+          "quantity": 0,
+          "size": "string",
+          "sku": "string",
+          "status": "string",
+          "totalGross": 0
+        }
+      ],
+      "expands": [
+        "string"
+      ],
+      "freightAmount": 0,
+      "invoice": {
+        "accessKey": "string",
+        "date": "2016-06-24T15:01:38.834Z",
+        "expands": [
+          "string"
+        ],
+        "links": [
+          {
+            "href": "string",
+            "rel": "string",
+            "templated": true
+          }
+        ],
+        "shipDate": "2016-06-24T15:01:38.835Z",
+        "url": "string"
+      },
+      "items": [
+        {
+          "brand": "string",
+          "color": "string",
+          "departmentCode": 0,
+          "departmentName": "string",
+          "discountUnitValue": 0,
+          "ean": "string",
+          "expands": [
+            "string"
+          ],
+          "flavor": "string",
+          "grossUnitValue": 0,
+          "itemId": 0,
+          "links": [
+            {
+              "href": "string",
+              "rel": "string",
+              "templated": true
+            }
+          ],
+          "manufacturerCode": "string",
+          "name": "string",
+          "netUnitValue": 0,
+          "quantity": 0,
+          "size": "string",
+          "sku": "string",
+          "status": "string",
+          "totalCommission": 0,
+          "totalDiscount": 0,
+          "totalFreight": 0,
+          "totalGross": 0,
+          "totalNet": 0
+        }
+      ],
+      "links": [
+        {
+          "href": "string",
+          "rel": "string",
+          "templated": true
+        }
+      ],
+      "sender": {
+        "expands": [
+          "string"
+        ],
+        "links": [
+          {
+            "href": "string",
+            "rel": "string",
+            "templated": true
+          }
+        ],
+        "sellerCode": 0,
+        "sellerName": "string",
+        "supplierCnpj": "string",
+        "supplierName": "string"
+      },
+      "shippingCode": 0,
+      "status": "string",
+      "transport": {
+        "carrier": "string",
+        "deliveryDate": "2016-06-24T15:01:38.836Z",
+        "deliveryService": "string",
+        "expands": [
+          "string"
+        ],
+        "links": [
+          {
+            "href": "string",
+            "rel": "string",
+            "templated": true
+          }
+        ],
+        "shipDate": "2016-06-24T15:01:38.836Z",
+        "trackingLink": "string",
+        "trackingNumber": "string",
+        "trackingShipDate": "2016-06-24T15:01:38.836Z"
+      }
+    }
+  ],
+  "totalCommission": 0,
+  "totalDiscount": 0,
+  "totalFreight": 0,
+  "totalGross": 0,
+  "totalNet": 0,
+  "totalQuantity": 0
+}
