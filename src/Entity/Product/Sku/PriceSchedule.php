cbd662f7c065683a3484e9c5e5829ae4748cb993
Gilmar Pupo <g@g1mr.com>
2016-06-28 14:28:03 -0300

    fix Price Schedule schema

--- src/Entity/Product/Sku/PriceSchedule.php
+++ src/Entity/Product/Sku/PriceSchedule.php
@@ -32,95 +32,15 @@ class PriceSchedule extends EntityAbstract implements EntityInterface
     public function getSchema()
     {
         return  [
-            'priceFrom' => 99.99,
-            'priceTo'   => 88.89,
-            'dateInit'  => '2016-06-24T15:01:38.826Z',
-            'dateEnd'   => '2016-06-24T15:01:38.826Z',
+            'priceFrom' => 'number',
+            'priceTo'   => 'number',
+            'dateInit'  => 'string',
+            'dateEnd'   => 'string',
         ];
     }
 
     protected function setUp()
     {
         $this->setOptionalSchema(['priceFrom', 'dateInit', 'dateEnd']);
-    }
-
-    /**
-     * @testdox Possui método ``getPriceFrom()`` para acessar PriceFrom
-     * @dataProvider dataProviderObject
-     * @test
-     */
-    public function getterPriceFrom(EntityInterface $object, $expected = null)
-    {
-        $this->assertSchemaGetter('priceFrom', 'number', $object, $expected);
-    }
-
-    /**
-     * @testdox Possui método ``setPriceFrom()`` que define PriceFrom
-     * @dataProvider dataProviderObject
-     * @test
-     */
-    public function setterPriceFrom(EntityInterface $object, $expected = null)
-    {
-        $this->assertSchemaSetter('priceFrom', 'number', $object);
-    }
-
-    /**
-     * @testdox Possui método ``getPriceTo()`` para acessar PriceTo
-     * @dataProvider dataProviderObject
-     * @test
-     */
-    public function getterPriceTo(EntityInterface $object, $expected = null)
-    {
-        $this->assertSchemaGetter('priceTo', 'number', $object, $expected);
-    }
-
-    /**
-     * @testdox Possui método ``setPriceTo()`` que define PriceTo
-     * @dataProvider dataProviderObject
-     * @test
-     */
-    public function setterPriceTo(EntityInterface $object, $expected = null)
-    {
-        $this->assertSchemaSetter('priceTo', 'number', $object);
-    }
-
-    /**
-     * @testdox Possui método ``getDateInit()`` para acessar DateInit
-     * @dataProvider dataProviderObject
-     * @test
-     */
-    public function getterDateInit(EntityInterface $object, $expected = null)
-    {
-        $this->assertSchemaGetter('dateInit', 'string', $object, $expected);
-    }
-
-    /**
-     * @testdox Possui método ``setDateInit()`` que define DateInit
-     * @dataProvider dataProviderObject
-     * @test
-     */
-    public function setterDateInit(EntityInterface $object, $expected = null)
-    {
-        $this->assertSchemaSetter('dateInit', 'string', $object);
-    }
-
-    /**
-     * @testdox Possui método ``getDateEnd()`` para acessar DateEnd
-     * @dataProvider dataProviderObject
-     * @test
-     */
-    public function getterDateEnd(EntityInterface $object, $expected = null)
-    {
-        $this->assertSchemaGetter('dateEnd', 'string', $object, $expected);
-    }
-
-    /**
-     * @testdox Possui método ``setDateEnd()`` que define DateEnd
-     * @dataProvider dataProviderObject
-     * @test
-     */
-    public function setterDateEnd(EntityInterface $object, $expected = null)
-    {
-        $this->assertSchemaSetter('dateEnd', 'string', $object);
     }
 }
