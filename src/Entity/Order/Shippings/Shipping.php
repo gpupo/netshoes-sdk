abdab06b6d9792d127d2611cf959f103300f216f
Gilmar Pupo <g@g1mr.com>
2016-06-29 10:23:26 -0300

    Testes para o objeto Shipping

--- src/Entity/Order/Shippings/Shipping.php
+++ src/Entity/Order/Shippings/Shipping.php
@@ -17,6 +17,30 @@ namespace Gpupo\NetshoesSdk\Entity\Order\Shippings;
 use Gpupo\CommonSdk\Entity\EntityAbstract;
 use Gpupo\CommonSdk\Entity\EntityInterface;
 
+/**
+ * @method int getShippingCode()    Acesso a shippingCode
+ * @method setShippingCode(integer $shippingCode)    Define shippingCode
+ * @method Gpupo\NetshoesSdk\Entity\Order\Shippings\Customer\Customer getCustomer()    Acesso a customer
+ * @method setCustomer(Gpupo\NetshoesSdk\Entity\Order\Shippings\Customer\Customer $customer)    Define customer
+ * @method float getFreightAmount()    Acesso a freightAmount
+ * @method setFreightAmount(float $freightAmount)    Define freightAmount
+ * @method Gpupo\NetshoesSdk\Entity\Order\Shippings\Invoice getInvoice()    Acesso a invoice
+ * @method setInvoice(Gpupo\NetshoesSdk\Entity\Order\Shippings\Invoice $invoice)    Define invoice
+ * @method Gpupo\NetshoesSdk\Entity\Order\Shippings\Items\Items getItems()    Acesso a items
+ * @method setItems(Gpupo\NetshoesSdk\Entity\Order\Shippings\Items\Items $items)    Define items
+ * @method Gpupo\NetshoesSdk\Entity\Order\Shippings\Sender getSender()    Acesso a sender
+ * @method setSender(Gpupo\NetshoesSdk\Entity\Order\Shippings\Sender $sender)    Define sender
+ * @method string getStatus()    Acesso a status
+ * @method setStatus(string $status)    Define status
+ * @method Gpupo\NetshoesSdk\Entity\Order\Shippings\Transport getTransport()    Acesso a transport
+ * @method setTransport(Gpupo\NetshoesSdk\Entity\Order\Shippings\Transport $transport)    Define transport
+ * @method string getCountry()    Acesso a country
+ * @method setCountry(string $country)    Define country
+ * @method string getCancellationReason()    Acesso a cancellationReason
+ * @method setCancellationReason(string $cancellationReason)    Define cancellationReason
+ * @method Gpupo\NetshoesSdk\Entity\Order\Shippings\DevolutionItems getDevolutionItems()    Acesso a devolutionItems
+ * @method setDevolutionItems(Gpupo\NetshoesSdk\Entity\Order\Shippings\DevolutionItems $devolutionItems)    Define devolutionItems
+ */
 class Shipping extends EntityAbstract implements EntityInterface
 {
     protected $primaryKey = 'shippingCode';
@@ -24,17 +48,17 @@ class Shipping extends EntityAbstract implements EntityInterface
     public function getSchema()
     {
         return [
-          'shippingCode'         => 'integer',
-          'customer'             => 'object',
-          'freightAmount'        => 'number',
-          'invoice'              => 'object',
-          'items'                => 'object',
-          'sender'               => 'object',
-          'status'               => 'string',
-          'transport'            => 'object',
-          'country'              => 'string',
-          'cancellationReason'   => 'string',
-          'devolutionItems'      => 'object',
+          'shippingCode'       => 'integer',
+          'customer'           => 'object',
+          'freightAmount'      => 'number',
+          'invoice'            => 'object',
+          'items'              => 'object',
+          'sender'             => 'object',
+          'status'             => 'string',
+          'transport'          => 'object',
+          'country'            => 'string',
+          'cancellationReason' => 'string',
+          'devolutionItems'    => 'object',
       ];
     }
 }
