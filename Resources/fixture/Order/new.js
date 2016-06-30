9f8bd2370e5aaf18cebdf62713095196386ea753
Gilmar Pupo <g@g1mr.com>
2016-06-30 11:02:00 -0300

    +fixture

--- Resources/fixture/Order/new.js
+++ Resources/fixture/Order/new.js
@@ -1,15 +1,15 @@
 {
-  "agreedDate": "2016-06-24T15:01:38.833Z",
-  "businessUnit": "string",
-  "devolutionRequested": true,
-  "exchangeRequested": true,
-  "orderDate": "2016-06-24T15:01:38.833Z",
-  "orderNumber": "string",
-  "orderStatus": "string",
-  "orderType": "string",
-  "originNumber": "string",
-  "originSite": "string",
-  "paymentDate": "2016-06-24T15:01:38.833Z",
+  "agreedDate": "2016-06-30T15:01:38.833Z",
+  "businessUnit": "ZT",
+  "devolutionRequested": false,
+  "exchangeRequested": false,
+  "orderDate": "2016-06-30T15:01:38.833Z",
+  "orderNumber": "111111",
+  "orderStatus": "Created",
+  "orderType": "Normal",
+  "originNumber": "22222222",
+  "originSite": "ZT",
+  "paymentDate": "2016-06-30T15:01:38.833Z",
   "shippings": [
     {
       "cancellationReason": "string",
@@ -33,83 +33,44 @@
         "stateInscription": "string",
         "tradeName": "string"
       },
-      "devolutionItems": [
-        {
-          "brand": "string",
-          "checkInData": "2016-06-24T15:01:38.834Z",
-          "color": "string",
-          "departmentCode": 0,
-          "departmentName": "string",
-          "devolutionData": "2016-06-24T15:01:38.834Z",
-          "devolutionExchangeStatus": "string",
-          "ean": "string",
-          "exchangeProcessCode": 0,
-          "flavor": "string",
-          "itemId": 0,
-          "manufacturerCode": "string",
-          "name": "string",
-          "quantity": 0,
-          "size": "string",
-          "sku": "string",
-          "status": "string",
-          "totalGross": 0
-        }
-      ],
       "freightAmount": 0,
-      "invoice": {
-        "accessKey": "string",
-        "date": "2016-06-24T15:01:38.834Z",
-        "shipDate": "2016-06-24T15:01:38.835Z",
-        "url": "string"
-      },
       "items": [
         {
-          "brand": "string",
-          "color": "string",
-          "departmentCode": 0,
-          "departmentName": "string",
+          "brand": "Nike",
+          "color": "Incolor",
+          "departmentCode": 9,
+          "departmentName": "Feminino",
           "discountUnitValue": 0,
-          "ean": "string",
-          "flavor": "string",
-          "grossUnitValue": 0,
-          "itemId": 0,
-          "manufacturerCode": "string",
-          "name": "string",
-          "netUnitValue": 0,
-          "quantity": 0,
-          "size": "string",
-          "sku": "string",
-          "status": "string",
+          "ean": "3380811244105",
+          "grossUnitValue": 10,
+          "itemId": 1,
+          "name": "Gentle Refiner Unissex 50ml Clarins",
+          "netUnitValue": 10,
+          "quantity": 1,
+          "size": "P",
+          "flavor": false,
+          "sku": "14080",
           "totalCommission": 0,
           "totalDiscount": 0,
           "totalFreight": 0,
-          "totalGross": 0,
-          "totalNet": 0
+          "totalGross": 10,
+          "totalNet": 10
         }
       ],
       "sender": {
-        "sellerCode": 0,
+        "sellerCode": 1,
         "sellerName": "string",
         "supplierCnpj": "string",
         "supplierName": "string"
       },
-      "shippingCode": 0,
-      "status": "string",
-      "transport": {
-        "carrier": "string",
-        "deliveryDate": "2016-06-24T15:01:38.836Z",
-        "deliveryService": "string",
-        "shipDate": "2016-06-24T15:01:38.836Z",
-        "trackingLink": "string",
-        "trackingNumber": "string",
-        "trackingShipDate": "2016-06-24T15:01:38.836Z"
-      }
+      "shippingCode": 1,
+      "status": "active"
     }
   ],
   "totalCommission": 0,
   "totalDiscount": 0,
   "totalFreight": 0,
-  "totalGross": 0,
-  "totalNet": 0,
-  "totalQuantity": 0
+  "totalGross": 10,
+  "totalNet": 10,
+  "totalQuantity": 1
 }
