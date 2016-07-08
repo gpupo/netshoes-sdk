195b3f97fff303c17abf58ed3135bd8d51c18f64
Gilmar Pupo <g@g1mr.com>
2016-07-08 14:15:41 -0300

    fix $existent = null

---
+++ src/Console/Command/ScreenplayCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of gpupo/netshoes-sdk
+ * Created by Gilmar Pupo <g@g1mr.com>
+ * For the information of copyright and license you should read the file
+ * LICENSE which is distributed with this source code.
+ * Para a informação dos direitos autorais e de licença você deve ler o arquivo
+ * LICENSE que é distribuído com este código-fonte.
+ * Para obtener la información de los derechos de autor y la licencia debe leer
+ * el archivo LICENSE que se distribuye con el código fuente.
+ * For more information, see <http://www.g1mr.com/>.
+ */
+
+namespace Gpupo\NetshoesSdk\Console\Command;
+
+use Closure;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class ScreenplayCommand extends AbstractCommand
+{
+    protected $list = [];
+
+
+
+
+}
