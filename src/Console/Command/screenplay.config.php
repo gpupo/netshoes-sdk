671cc14159b029a2e0b4ae1989227d692007209b
Gilmar Pupo <g@g1mr.com>
2016-07-08 17:30:28 -0300

    screenplay.template

---
+++ src/Console/Command/screenplay.config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'product:post:simple' => 'Cadastro de quatro produtos contendo apenas um Sku',
+    'product:post:multiple'=>'Cadastro de quatro produtos contendo mais de um Sku',
+    'product:update'=>'Atualizar parcialmente no mínimo quatro produtos criados. As alterações devem incluir department, productType e attibutes',
+    'product:update:full'=> 'Atualizar três produtos cadastrados. As atualizações devem mudar o produto por completo exceto ID e SKU',
+];
