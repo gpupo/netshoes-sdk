6eff85742fba44b4e6e4e9b42dcfbafaee8875c2
Gilmar Pupo <g@g1mr.com>
2016-07-11 09:00:47 -0300

    list OK

--- src/Console/Command/screenplay.template.php
+++ src/Console/Command/screenplay.template.php
@@ -1,9 +1,16 @@
 <?php
 
-$output->writeln('Este roteiro precisa ser implementado!');
+/*
+ * This file is part of gpupo/netshoes-sdk
+ * Created by Gilmar Pupo <g@g1mr.com>
+ * For the information of copyright and license you should read the file
+ * LICENSE which is distributed with this source code.
+ * Para a informação dos direitos autorais e de licença você deve ler o arquivo
+ * LICENSE que é distribuído com este código-fonte.
+ * Para obtener la información de los derechos de autor y la licencia debe leer
+ * el archivo LICENSE que se distribuye con el código fuente.
+ * For more information, see <http://www.g1mr.com/>.
+ */
 
-
-//Exemplos de uso:
-
-//$product = $sdk->createProduct($data);
-//$sdk->factoryManager('product')->save($product);
+$output->writeln('Este roteiro contém as variáveis $sdk, $output, $app');
+$output->writeln('<options=bold> ====> Este roteiro ainda não foi implementado!</>');
