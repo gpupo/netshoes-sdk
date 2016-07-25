9c0211bb0f6e914b76942a9a1a7b63e975b82158
Gilmar Pupo <g@g1mr.com>
2016-07-25 11:23:17 -0300

    Testtes para Order Translator

---
+++ tests/Entity/Order/TranslatorTest.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+ * This file is part of gpupo/netshoes-sdk
+ * Created by Gilmar Pupo <g@g1mr.com>
+ * For the information of copyright and license you should read the file
+ * LICENSE which is distributed with this source code.
+ * Para a informação dos direitos autorais e de licença você deve ler o arquivo
+ * LICENSE que é distribuído com este código-fonte.
+ * Para obtener la información de los derechos de autor y la licencia debe leer
+ * el archivo LICENSE que se distribuye con el código fuente.
+ * For more information, see <http://www.g1mr.com/>.
+ */
+
+namespace Gpupo\Tests\NetshoesSdk\Entity\Order;
+
+use Gpupo\CommonSchema\TranslatorDataCollection;
+use Gpupo\NetshoesSdk\Entity\Order\Order;
+use Gpupo\NetshoesSdk\Entity\Order\Translator;
+use Gpupo\Tests\NetshoesSdk\TestCaseAbstract;
+
+/**
+ * @coversDefaultClass \Gpupo\NetshoesSdk\Entity\Order\Translator
+ */
+class TranslatorTest extends TestCaseAbstract
+{
+    /**
+     * @return \Gpupo\NetshoesSdk\Entity\Order\Translator
+     */
+    public function dataProviderTranslator()
+    {
+        $list = [];
+
+        foreach ($this->providerOrders() as $order) {
+            $translator = new Translator(['native' => $order]);
+
+            $list[] = [$translator];
+        }
+
+        return $list;
+    }
+
+    /**
+     * @testdox ``translateTo()``
+     * @cover ::translateTo
+     * @dataProvider dataProviderTranslator
+     * @test
+     */
+    public function translateTo(Translator $translator)
+    {
+        $translated = $translator->translateTo();
+        $this->assertInstanceOf(TranslatorDataCollection::class, $translated);
+        $this->assertInternalType('array', $translated->toArray(), 'internal type');
+    }
+
+    /**
+     * @testdox ``translateFrom()``
+     * @cover ::translateFrom
+     * @dataProvider dataProviderTranslator
+     * @test
+     */
+    public function translateFrom(Translator $translator)
+    {
+        $foreign = $translator->translateTo();
+        $this->assertInstanceOf(TranslatorDataCollection::class, $foreign);
+        $translator->setForeign($foreign);
+        $translated = $translator->translateFrom();
+        $this->assertInstanceOf(Order::class, $translated);
+        $this->assertInternalType('array', $translated->toArray(), 'internal type');
+    }
+
+    /**
+     * @testdox ``Traduz sem perder informação``
+     * @cover ::translateFrom
+     * @cover ::translateTo
+     * @dataProvider dataProviderTranslator
+     * @test
+     */
+    public function translateViceVersa(Translator $translator)
+    {
+        $order = $translator->getNative();
+        $this->assertInstanceOf(Order::class, $order);
+        $foreign = $translator->translateTo();
+        $translator->setForeign($foreign);
+        $translated = $translator->translateFrom();
+
+        $this->assertEquals($order->getId(), $translated->getId());
+    }
+}
