5a7510a0f784f96f1bdcab3216e50bce948f22f0
Gilmar Pupo <g@g1mr.com>
2016-08-09 15:40:43 -0300

    Status

---
+++ src/Entity/Product/Status.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of gpupo/netshoes-sdk
+ * Created by Gilmar Pupo <g@g1mr.com>
+ * For the information of copyright and license you should read the file
+ * LICENSE which is distributed with this source code.
+ * Para a informação dos direitos autorais e de licença você deve ler o arquivo
+ * LICENSE que é distribuído com este código-fonte.
+ * Para obtener la información de los derechos de autor y la licencia debe leer
+ * el archivo LICENSE que se distribuye con el código fuente.
+ * For more information, see <http://www.g1mr.com/>.
+ */
+
+namespace Gpupo\NetshoesSdk\Entity\Product;
+
+use Gpupo\CommonSdk\Entity\EntityAbstract;
+use Gpupo\CommonSdk\Entity\EntityInterface;
+
+/**
+ */
+final class Status extends EntityAbstract implements EntityInterface
+{
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getSchema()
+    {
+        return [
+            'active'          => 'boolean',
+            'skuSynchronized' => 'boolean',
+            'statusMatch'     => 'string',
+            'hasPrice'        => 'boolean',
+            'hasStock'        => 'boolean',
+        ];
+    }
+
+    public function isPending()
+    {
+    }
+}
