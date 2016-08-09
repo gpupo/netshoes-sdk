257b367e828af5c174bd91b6250e51cf538436a8
Gilmar Pupo <g@g1mr.com>
2016-08-09 15:40:43 -0300

    Status

---
+++ tests/Entity/Product/StatusTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of gpupo/netshoes-sdk
+ * Created by Gilmar Pupo <g@g1mr.com>
+ * For the information of copyright and license you should read the file
+ * LICENSE which is distributed with this source code.
+ * Para a informação dos direitos autorais e de licença você deve ler o arquivo
+ * LICENSE que é distribuído com este código-fonte.
+ * Para obtener la información de los derechos de autor y la licencia debe leer
+ * el archivo LICENSE que se distribuye con el código fuente.
+ * For more information, see <http://www.g1mr.com/>.
+ */
+
+namespace Gpupo\Tests\NetshoesSdk\Entity\Product;
+
+use Gpupo\NetshoesSdk\Entity\Product\Status;
+use Gpupo\Tests\NetshoesSdk\TestCaseAbstract;
+
+/**
+ * @coversDefaultClass \Gpupo\NetshoesSdk\Entity\Product\Status
+ */
+class StatusTest extends TestCaseAbstract
+{
+    /**
+     * @testdox Identifica se um produto está pendente
+     */
+    public function isPending()
+    {
+        $status = new Status([
+            'active'      => true,
+            'statusMatch' => 'PROCESSADO_INTEGRACAO_CATALOGO',
+        ]);
+
+        $this->assertTrue($status->isPending());
+    }
+}
